8275106b107534f3195e9298abd4a4131c102642
Hani Alsafadi <hani.ns.safadi@gmail.com>
2021-04-21 05:43:55 +0200

    TEST

---
+++ _site/assets/js/code.js
@@ -0,0 +1,35 @@
+
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+}
+function updateMeter(){
+$(".meter > span").each(function () {
+  var x = document.getElementById("goal").value;
+  var y = document.getElementById("current").value;
+  var z = 100 * y / x;
+  $('#htext').text('With your help we have reached ' + Math.round(z) + '% of our goal: ' + numberWithCommas($('#goal').val()) + ' SEK' );
+  $(this)
+      .data("origWidth", $(this).width())
+      .width(0)
+      .animate(
+        {
+          width: z+"%"
+        },
+        1200
+      );
+  });
+};
+
+updateMeter();
+
+
+
+  function makeImage(){
+    html2canvas(document.querySelector("#capture")).then(canvas => {
+      document.body.appendChild(canvas)
+  });
+  }
+
+  function updatetext(){
+  
+  }
