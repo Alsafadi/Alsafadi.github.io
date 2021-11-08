5b3bcf4fea39cae47221157b449eb9bd619aa71c
Hani Alsafadi <hani.ns.safadi@gmail.com>
2021-11-08 21:07:15 +0100

    updates11-08

--- _site/assets/js/main.js
+++ _site/assets/js/main.js
@@ -1,3 +1,4 @@
+
 function toggleDisplay(X,Y){
     let ids = [...document.querySelectorAll('[id^="'+X+'-"]')].map(item => item.id);
 ids.forEach(toggle)    
@@ -6,8 +7,12 @@ function toggle(value, index, array){
 d = document.getElementById(value);
 if(index>Y && window.getComputedStyle(d).display=="block") {
     document.getElementById(value).style.display = "none";
+    var idd = "toggle-"+X;
+    document.getElementById(idd).innerHTML = "Show More!";
 }else{
     document.getElementById(value).style.display = "block";
+    var idd = "toggle-"+X;
+    document.getElementById(idd).innerHTML = "Show less!";
 }
 }
 }
