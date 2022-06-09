5a42b9b5d19bebf678b3617dda3a1bcfc3b58c5f
Hani Alsafadi <hani.ns.safadi@gmail.com>
2022-06-09 15:26:46 +0200

    Added portfolio

---
+++ _includes/intro.md
@@ -0,0 +1,10 @@
+# PhD Portfolio - 2022
+
+### About me
+I was born and raised in Gaza, Palestine. I left home at the age of 17 to pursue my education abroad starting with the international baccalaureate (IB) in Italy followed by bachelor's degree in the USA and master courses in Germany. I was admitted to the PhD course in August of 2018 at the group of Dr. Darcy Wagner, Lung bioengineering and regeneration (LBR). My PhD project is to explore the context dependence of the co-transcriptional factors Yap and Taz in in the lung epithelium of Idiopathic Pulmonary Fibrosis (IPF). 
+
+### About the portfolio
+
+This portfolio is a form of documentation of the fullfilment of the learning outcomes of the PhD. It covers various reflections in all the different aspects of the PhD. The portfolio is intented to promote and enhance reflection on the different outcomes of the PhD degree. The learning outcomes of the PhD are categorized in 12 categories; each entry in these sections below represents either a reflection on the learning outcomes or a documentation of a specific event/activity pertaining to the PhD. The entries are not ordered chronologically, however, they are ordered in a sequence that suites the content.
+
+I hope that by reading this portfolio, you will have an insight into my journey through the PhD.
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,9 @@
 
 function toggleDisplay(X,Y){
     let ids = [...document.querySelectorAll('[id^="'+X+'-"]')].map(item => item.id);
-ids.forEach(toggle)    
+ids.forEach(togglee)    
 
-function toggle(value, index, array){
+function togglee(value, index, array){
 d = document.getElementById(value);
 if(index>Y && window.getComputedStyle(d).display=="block") {
     document.getElementById(value).style.display = "none";
@@ -46,3 +46,37 @@ WinPrint.print()
 WinPrint.close();
 
 }
+
+function toggleEntry(z){
+
+    d = document.getElementById("c"+z);
+    
+    if(window.getComputedStyle(d).display=="block") {
+        document.getElementById("c"+z).style.display = "none";
+        const iid = "i"+z
+        document.getElementById(iid).className = "fa-solid fa-circle-chevron-right";
+    
+    }else{
+        document.getElementById("c"+z).style.display = "block";
+        const iid = "i"+z
+        document.getElementById(iid).className = "fa-solid fa-circle-chevron-down";
+    }
+    }
+    
+
+    // When the user scrolls down 20px from the top of the document, show the button
+    window.onscroll = function() {scrollFunction()};
+    
+    function scrollFunction() {
+      if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+      } else {
+        document.getElementById("myBtn").style.display = "none";
+      }
+    }
+    
+    // When the user clicks on the button, scroll to the top of the document
+    function topFunction() {
+      document.body.scrollTop = 0;
+      document.documentElement.scrollTop = 0;
+    }
